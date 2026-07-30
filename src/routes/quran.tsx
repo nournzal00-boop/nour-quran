8873b61965409cf382b9a0814de0b86b8e2f0efa
nournzal00-boop <nournzal00@gmail.com>
2026-07-30 11:44:45 +0300

    Update quran.tsx

--- src/routes/quran.tsx
+++ src/routes/quran.tsx
@@ -1,5 +1,10 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute } from '@tanstack/react-router'
+import { QuranReaderView } from '../components/quran-reader-view'
 
-export const Route = createFileRoute("/quran")({
-  component: () => <Outlet />,
-});
+export const Route = createFileRoute('/quran')({
+  component: QuranRoute,
+})
+
+function QuranRoute() {
+  return <QuranReaderView />
+}
